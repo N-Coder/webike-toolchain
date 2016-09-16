cf4b09ac7975e9e60209faa642acde769da944a0
Niko Fink <nfink95@gmail.com>
2016-09-16 15:57:15 -0400

    Separated preprocessing and generating histograms for trips

--- Main.py
+++ Main.py
@@ -1,91 +1,136 @@
-import os
-
 import matplotlib.pyplot as plt
-import pymysql
+import numpy as np
 
+import Weather
 from Constants import IMEIS
-from Weather import append_hist, plot_weather, SQL_MAPPING, extract_hist, read_data_db
 
 __author__ = 'Niko Fink'
 
-connection = pymysql.connect(
-    host="tornado.cs.uwaterloo.ca",
-    port=3306,
-    user=os.environ['MYSQL_USER'],
-    passwd=os.environ['MYSQL_PASSWORD'],
-    db="webike"
-)
-cursor = connection.cursor(pymysql.cursors.DictCursor)
 
-start_times = []
-distances = []
-initial_soc = []
-final_soc = []
-trip_weather = {}
-for k, v in SQL_MAPPING.items():
-    trip_weather[v] = []
+def preprocess_trips(cursor):
+    print('Preprocessing JOIN information for new trips')
+    try:
+        for imei in IMEIS:
+            cursor.execute("SELECT trip{imei}.* FROM trip{imei} LEFT JOIN webike_sfink.trips ON "
+                           "trip{imei}.id = trips.trip AND trips.imei='{imei}' WHERE trips.trip IS NULL;"
+                           .format(imei=imei))
+            unprocessed_trips = cursor.fetchall()
+            print('Processing {} new entries for IMEI {}'.format(len(unprocessed_trips), imei))
+            for nr, trip in enumerate(unprocessed_trips):
+                print('{} of {}: processing new Trip {}#{}'
+                      .format(nr + 1, len(unprocessed_trips), imei, trip['id']))
 
-for imei in IMEIS:
-    print('Processing IMEI ' + imei)
+                # unfortunately, we can't use prepared statements as the table names change
+                # (table and column names have to be static in SQL)
+                cursor.execute("SELECT Stamp FROM imei{} WHERE Stamp > '{}' ORDER BY Stamp ASC LIMIT 1"
+                               .format(imei, trip['start_time']))
+                first_sample = cursor.fetchone()
+                cursor.execute("SELECT Stamp FROM imei{} WHERE Stamp < '{}' ORDER BY Stamp DESC LIMIT 1"
+                               .format(imei, trip['end_time']))
+                last_sample = cursor.fetchone()
+                cursor.execute("SELECT datetime AS diff "
+                               "FROM webike_sfink.weather ORDER BY diff LIMIT 1"
+                               .format(trip['start_time']))
+                weather_sample = cursor.fetchone()
 
-    cursor.execute("SELECT * FROM trip%s" % imei)
-    trips = cursor.fetchall()
-    for trip in trips:
-        print('Processing Trip ' + imei + '#' + str(trip['id']))
+                res = cursor.execute(
+                    "INSERT INTO webike_sfink.trips(imei, trip, start_time, end_time, distance, weather) VALUES "
+                    "(%s,%s,%s,%s,%s,%s)",
+                    (imei, trip['id'], first_sample['Stamp'], last_sample['Stamp'], trip['distance'],
+                     weather_sample['datetime']))
+                assert res == 1
+        connection.commit()
+    except:
+        connection.rollback()
+        raise
 
-        # unfortunately, we can't use prepared statements as the table names change
-        # (table and column names have to be static in SQL)
-        cursor.execute("SELECT * FROM imei{} WHERE Stamp > '{}' ORDER BY Stamp ASC LIMIT 1"
-                       .format(imei, trip['start_time']))
-        first_sample = cursor.fetchone()
-        cursor.execute("SELECT * FROM imei{} WHERE Stamp < '{}' ORDER BY Stamp DESC LIMIT 1"
-                       .format(imei, trip['end_time']))
-        last_sample = cursor.fetchone()
-        cursor.execute("SELECT *, ABS(TIMESTAMPDIFF(SECOND, datetime, '{}')) AS diff "
-                       "FROM webike_sfink.weather ORDER BY diff LIMIT 1"
-                       .format(trip['start_time']))
-        weather_sample = cursor.fetchone()
 
-        start_times.append(first_sample['Stamp'].replace(year=2000, month=1, day=1))
-        if trip['distance'] is not None:
-            distances.append(float(trip['distance']))
-        if first_sample['ChargingCurr'] is not None:
-            initial_soc.append(float(first_sample['ChargingCurr']))  # TODO fix range
-        if last_sample['ChargingCurr'] is not None:
-            final_soc.append(float(last_sample['ChargingCurr']))  # TODO fix range
-        for key, val in weather_sample.items():
-            append_hist(trip_weather, key, val)
+def extract_hist(qcursor):
+    print('Generating histogram data')
 
-print('Plotting graphs')
-figcnt = 0
-plt.figure(figcnt)
-plt.hist(start_times, bins=24)
-plt.xlabel('Time of Day')
-plt.ylabel('Number of Trips')
-plt.title('Number of Trips per Hour of Day')
-plt.savefig('out/trips_per_hour.png')
+    hist_data = {
+        'start_times': [],
+        'distances': [],
+        'initial_soc': [],
+        'final_soc': [],
+        'trip_weather': {}
+    }
+    for k, v in Weather.SQL_MAPPING.items():
+        hist_data['trip_weather'][v] = []
 
-figcnt += 1
-plt.figure(figcnt)
-plt.hist(distances, bins=25)
-plt.xlabel('Distance')
-plt.ylabel('Number of Trips')
-plt.title('Number of Trips per Distance')
-plt.savefig('out/trips_per_distance.png')
+    for imei in IMEIS:
+        print('Processing IMEI ' + imei)
 
-figcnt += 1
-plt.figure(figcnt)
-plt.hist(initial_soc, bins=25, alpha=0.5)
-plt.hist(final_soc, bins=25, alpha=0.5)
-plt.xlabel('SoC')
-plt.ylabel('Number of Trips')
-plt.title('Number of Trips with certain Initial and Final State of Charge')
-plt.savefig('out/trips_per_soc.png')
+        qcursor.execute(
+            "SELECT * "
+            "FROM webike_sfink.trips trip "
+            "  JOIN imei{imei} first_sample ON first_sample.Stamp = trip.start_time "
+            "  JOIN imei{imei} last_sample ON last_sample.Stamp = trip.end_time "
+            "  JOIN webike_sfink.weather weather ON trip.weather = weather.datetime "
+            "WHERE trip.imei = '{imei}'".format(imei=imei))
+        trips = qcursor.fetchall()
+        for trip in trips:
+            print('Processing trip {}#{}'.format(imei, trip['trip.trip']))
 
-average_weather = extract_hist(read_data_db(cursor))
-plot_weather(average_weather, fig_offset=figcnt, facecolor='green', alpha=0.5, normed=True, label_prefix='average-')
-figcnt = plot_weather(trip_weather, out_file='out/trips_per_weather_{}.png', fig_offset=figcnt, facecolor='blue',
-                      alpha=0.5, normed=True, label_prefix='trip-')
+            hist_data['start_times'].append(trip['first_sample.Stamp'].replace(year=2000, month=1, day=1))
 
-# plt.show()
-connection.close()
+            if trip['trip.distance'] is not None:
+                hist_data['distances'].append(float(trip['trip.distance']))
+
+            if trip['first_sample.ChargingCurr'] is not None:
+                hist_data['initial_soc'].append(float(trip['first_sample.ChargingCurr']))  # TODO fix range
+
+            if trip['last_sample.ChargingCurr'] is not None:
+                hist_data['final_soc'].append(float(trip['last_sample.ChargingCurr']))  # TODO fix range
+
+            weather_prefix = 'weather.'
+            for key, val in trip.items():
+                if key.startswith(weather_prefix):
+                    Weather.append_hist(hist_data['trip_weather'], key[len(weather_prefix):], val)
+
+    return hist_data
+
+
+def plot_trips(hist_data):
+    print('Plotting trip graphs')
+    plt.clf()
+    plt.hist(hist_data['start_times'], bins=24)
+    plt.xlabel('Time of Day')
+    plt.ylabel('Number of Trips')
+    plt.title('Number of Trips per Hour of Day')
+    plt.savefig('out/trips_per_hour.png')
+
+    plt.clf()
+    plt.hist(hist_data['distances'], bins=25)
+    plt.xlabel('Distance')
+    plt.ylabel('Number of Trips')
+    plt.title('Number of Trips per Distance')
+    plt.savefig('out/trips_per_distance.png')
+
+    plt.clf()
+    bins = np.linspace(
+        min(hist_data['initial_soc'] + hist_data['final_soc']),
+        max(hist_data['initial_soc'] + hist_data['final_soc']), 30)
+    plt.hist(hist_data['initial_soc'], bins=bins, alpha=0.5, label='initial')
+    plt.hist(hist_data['final_soc'], bins=bins, alpha=0.5, label='final')
+    plt.xlabel('SoC')
+    plt.ylabel('Number of Trips')
+    plt.title('Number of Trips with certain Initial and Final State of Charge')
+    plt.savefig('out/trips_per_soc.png')
+
+
+if __name__ == "__main__":
+    from DB import qcursor, cursor, connection
+
+    preprocess_trips(cursor)
+    trip_hist_data = extract_hist(qcursor)
+    plot_trips(trip_hist_data)
+
+    # average_weather = Weather.extract_hist(Weather.read_data_db(cursor))
+    # Weather.plot_weather(average_weather, fig_offset=figcnt, facecolor='green', alpha=0.5, normed=True,
+    #                      label_prefix='average-')
+    # figcnt = Weather.plot_weather(trip_weather, out_file='out/trips_per_weather_{}.png', fig_offset=figcnt,
+    #                               facecolor='blue',
+    #                               alpha=0.5, normed=True, label_prefix='trip-')
+
+    connection.close()
