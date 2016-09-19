46465d8388bffff0de4a348bd2f1c7a306ca47c1
Niko Fink <nfink95@gmail.com>
2016-09-19 17:35:06 -0400

    plot metar data

--- Main.py
+++ Main.py
@@ -1,6 +1,8 @@
 import DB
 import Trips
+import Weather
 import WeatherGC
+import WeatherWU
 
 __author__ = 'Niko Fink'
 assert __name__ == "__main__"
@@ -11,11 +13,20 @@ with DB.connect() as connection:
 
     gc_db_data = WeatherGC.read_data_db(connection)
     gc_hist_data = WeatherGC.extract_hist(gc_db_data)
-    WeatherGC.plot_weather(
+    Weather.plot_weather(
         {
-            # 'metar': # TODO plot WU
             'weather': gc_hist_data,
             'trip': trip_hist_data['trip_weather']
         },
-        out_file='out/trips_per_weather_{}.png'
+        out_file='out/trips_per_weathergc_{}.png'
+    )
+
+    wu_db_data = WeatherWU.read_data_db(connection)
+    wu_hist_data = WeatherWU.extract_hist(wu_db_data)
+    Weather.plot_weather(
+        {
+            'weather': wu_hist_data,
+            'trip': trip_hist_data['trip_metar']
+        },
+        out_file='out/trips_per_wunderg_{}.png'
     )
