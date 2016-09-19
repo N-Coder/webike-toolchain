0d13fab3eb33087bc55948bf66a384ae645de35d
Niko Fink <nfink95@gmail.com>
2016-09-19 17:34:58 -0400

    include metar data in trip hists

--- Trips.py
+++ Trips.py
@@ -1,9 +1,15 @@
+import copy
+
 import matplotlib.pyplot as plt
 import numpy as np
 
 import WeatherGC
+import WeatherWU
 from Constants import IMEIS
 from DB import DictCursor, QualifiedDictCursor
+
+HIST_DATA = {'start_times': [], 'distances': [], 'initial_soc': [], 'final_soc': [],
+             'trip_weather': copy.deepcopy(WeatherGC.HIST_DATA), 'trip_metar': copy.deepcopy(WeatherWU.HIST_DATA)}
 
 __author__ = 'Niko Fink'
 
@@ -29,16 +35,22 @@ def preprocess_trips(connection):
                 cursor.execute("SELECT Stamp FROM imei{} WHERE Stamp < '{}' ORDER BY Stamp DESC LIMIT 1"
                                .format(imei, trip['end_time']))
                 last_sample = cursor.fetchone()
-                cursor.execute("SELECT datetime, ABS(TIMESTAMPDIFF(SECOND, datetime, '{}')) AS diff "
-                               "FROM webike_sfink.weather ORDER BY diff LIMIT 1"
-                               .format(trip['start_time']))
+                cursor.execute(
+                    "SELECT datetime, ABS(TIMESTAMPDIFF(SECOND, datetime, '{}')) AS diff "  # FIMXE check time zone
+                    "FROM webike_sfink.weather ORDER BY diff LIMIT 1"
+                        .format(trip['start_time']))
                 weather_sample = cursor.fetchone()
+                cursor.execute(
+                    "SELECT stamp, ABS(TIMESTAMPDIFF(SECOND, stamp, '{}')) AS diff "  # FIMXE check time zone
+                    "FROM webike_sfink.weather_metar ORDER BY diff LIMIT 1"
+                        .format(trip['start_time']))
+                metar_sample = cursor.fetchone()
 
                 res = cursor.execute(
-                    "INSERT INTO webike_sfink.trips(imei, trip, start_time, end_time, distance, weather) VALUES "
-                    "(%s,%s,%s,%s,%s,%s)",
+                    "INSERT INTO webike_sfink.trips(imei, trip, start_time, end_time, distance, weather, metar) VALUES "
+                    "(%s,%s,%s,%s,%s,%s,%s)",
                     (imei, trip['id'], first_sample['Stamp'], last_sample['Stamp'], trip['distance'],
-                     weather_sample['datetime']))
+                     weather_sample['datetime'], metar_sample['stamp']))
                 if res != 1:
                     raise AssertionError("Illegal result {} for row #{}: {}".format(res, nr, trip))
 
@@ -47,13 +59,7 @@ def extract_hist(connection):
     print('Generating histogram data')
 
     with connection.cursor(QualifiedDictCursor) as qcursor:
-        hist_data = {
-            'start_times': [],
-            'distances': [],
-            'initial_soc': [],
-            'final_soc': [],
-            'trip_weather': {}
-        }
+        hist_data = copy.deepcopy(HIST_DATA)
         for k, v in WeatherGC.SQL_MAPPING.items():
             hist_data['trip_weather'][v] = []
 
@@ -63,9 +69,12 @@ def extract_hist(connection):
             qcursor.execute(
                 "SELECT * "
                 "FROM webike_sfink.trips trip "
-                "  JOIN imei{imei} first_sample ON first_sample.Stamp = trip.start_time "
-                "  JOIN imei{imei} last_sample ON last_sample.Stamp = trip.end_time "
-                "  JOIN webike_sfink.weather weather ON trip.weather = weather.datetime "
+                "  LEFT OUTER JOIN imei{imei} first_sample ON first_sample.Stamp = trip.start_time "
+                "  LEFT OUTER JOIN imei{imei} last_sample ON last_sample.Stamp = trip.end_time "
+                "  LEFT OUTER JOIN webike_sfink.soc first_soc ON first_soc.time = trip.start_time AND first_soc.imei = '{imei}' "
+                "  LEFT OUTER JOIN webike_sfink.soc last_soc ON last_soc.time = trip.end_time AND last_soc.imei = '{imei}' "
+                "  LEFT OUTER JOIN webike_sfink.weather weather ON trip.weather = weather.datetime "
+                "  LEFT OUTER JOIN webike_sfink.weather_metar metar ON trip.metar = metar.stamp "
                 "WHERE trip.imei = '{imei}'".format(imei=imei))
             trips = qcursor.fetchall()
             for trip in trips:
@@ -74,16 +83,20 @@ def extract_hist(connection):
                 if trip['trip.distance'] is not None:
                     hist_data['distances'].append(float(trip['trip.distance']))
 
-                if trip['first_sample.ChargingCurr'] is not None:
-                    hist_data['initial_soc'].append(float(trip['first_sample.ChargingCurr']))  # TODO fix range
+                if trip['first_soc.soc_smooth'] is not None:
+                    hist_data['initial_soc'].append(float(trip['first_soc.soc_smooth']))
 
-                if trip['last_sample.ChargingCurr'] is not None:
-                    hist_data['final_soc'].append(float(trip['last_sample.ChargingCurr']))  # TODO fix range
+                if trip['last_soc.soc_smooth'] is not None:
+                    hist_data['final_soc'].append(float(trip['last_soc.soc_smooth']))
 
                 weather_prefix = 'weather.'
                 for key, val in trip.items():
                     if key.startswith(weather_prefix):
                         WeatherGC.append_hist(hist_data['trip_weather'], key[len(weather_prefix):], val)
+
+                for key, val in trip.items():
+                    if key.startswith('metar.'):
+                        WeatherWU.append_hist(hist_data['trip_metar'], val)
 
         return hist_data
 
@@ -108,10 +121,24 @@ def plot_trips(hist_data):
     bins = np.linspace(
         min(hist_data['initial_soc'] + hist_data['final_soc']),
         max(hist_data['initial_soc'] + hist_data['final_soc']), 30)
-    plt.hist(hist_data['initial_soc'], bins=bins, alpha=0.5, label='initial')
-    plt.hist(hist_data['final_soc'], bins=bins, alpha=0.5, label='final')
+    hist_initial = plt.hist(hist_data['initial_soc'], bins=bins, label='initial')
+    hist_final = plt.hist(hist_data['final_soc'], bins=bins, label='final')
+    order_hists([hist_initial, hist_final])
     plt.xlabel('SoC')
     plt.ylabel('Number of Trips')
     plt.title('Number of Trips with certain Initial and Final State of Charge')
-    plt.legend()
+    plt.legend(loc='upper left')
     plt.savefig('out/trips_per_soc.png')
+
+
+def order_hists(hists):
+    """see http://stackoverflow.com/a/8764575/805569"""
+    all_ns = [hist[0] for hist in hists]
+    all_patches = [hist[2] for hist in hists]
+
+    z_orders = -np.argsort(all_ns, axis=0)
+
+    for zrow, patchrow in zip(z_orders, all_patches):
+        assert len(zrow) == len(patchrow)
+        for z_val, patch in zip(zrow, patchrow):
+            patch.set_zorder(z_val)
