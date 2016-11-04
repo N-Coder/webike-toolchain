4e410142ef93003fd50bc20b032515ee4d95ab56
Niko Fink <nfink95@gmail.com>
2016-11-04 18:09:24 -0400

    add default logging config

--- webike/util/Logging.py
+++ webike/util/Logging.py
@@ -1,3 +1,8 @@
+import logging
+import logging.config
+
+import yaml
+
 __author__ = "Niko Fink"
 
 
@@ -9,3 +14,31 @@ class BraceMessage(object):
 
     def __str__(self):
         return self.fmt.format(*self.args, **self.kwargs)
+
+
+def default_logging_config():
+    # TODO load from file
+    logging_config = yaml.load("""
+    version: 1
+    handlers:
+      console:
+        class: logging.StreamHandler
+        formatter: default
+        level: DEBUG
+        stream: ext://sys.stdout
+      file:
+        class : logging.handlers.RotatingFileHandler
+        formatter: default
+        filename: logconfig.log
+        maxBytes: 1024
+        backupCount: 3
+    loggers:
+      urllib3:
+        level: WARNING
+      requests:
+        level: WARNING
+    formatters:
+      default:
+        format: "%(asctime)s %(levelname)-3.3s %(name)-12.12s - %(message)s"
+    """)
+    logging.config.dictConfig(logging_config)
